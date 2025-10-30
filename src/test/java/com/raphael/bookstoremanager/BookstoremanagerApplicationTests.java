ff04eddd1254f46236218cdfa53423580b78a0ed
Rafael Fortunato <fortunatoraphael@hotmail.com>
2025-10-29 22:10:13 -0300

    First commit: estrutura do projeto criada

---
+++ src/test/java/com/raphael/bookstoremanager/BookstoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raphael.bookstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
